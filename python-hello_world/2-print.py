f56bfee0b02773f62ffc21296dfce0d1e39fe48c
Travis Adamson <tndadamson@gmail.com>
2023-09-05 08:04:22 -0700

    Erasing almost everything

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #1/usr/bin/python3
-def main ():
-    """This function prints a given statement using the print function"""
-
-    print("\"Programming is like building a multilingual puzzle")
-main()
+print("\"Programming is like building a multilingual puzzle")
