9a322de5f6e8a25f9b8603488b93e7ca03282e24
Travis Adamson <tndadamson@gmail.com>
2023-09-05 12:47:54 -0700

    Added the end to the first print to remove \n

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print(f"Last digit of {number:d} is {last_digit:d}")
+print(f"Last digit of {number:d} is {last_digit:d}", end="")
 if last_digit > 5:
     print(" and is greater than 5")
 if last_digit == 0:
