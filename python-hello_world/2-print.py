96d3c21e4454121094809f0d161c1a91f1416f15
Travis Adamson <tndadamson@gmail.com>
2023-09-05 07:56:07 -0700

    Remvoed \n

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,5 +1,5 @@
 def main ():
     """This function prints a given statement using the print function"""
 
-    print("\"Programming is like building a multilingual puzzle\n")
+    print("\"Programming is like building a multilingual puzzle")
 main()
