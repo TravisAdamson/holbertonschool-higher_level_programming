d9675e0f6f8db5257f5b1481bc2119292d84541b
Travis Adamson <tndadamson@gmail.com>
2023-09-05 07:53:47 -0700

    Removed if main line

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -2,7 +2,4 @@ def main ():
     """This function prints a given statement using the print function"""
 
     print("\"Programming is like building a multilingual puzzle\n")
-
-if __name__ == "__main__":
-    """This makes sure that it only prints the statement if it is run directly"""
-    main()
+main()
