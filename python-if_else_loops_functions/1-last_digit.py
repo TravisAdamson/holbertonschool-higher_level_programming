99875916adb439200b9d0dc1f651bc95d357cde2
Travis Adamson <tndadamson@gmail.com>
2023-09-05 12:45:38 -0700

    Fixed absolute value need

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-print(ff"Last digit of {number:d} is {last_digit:d}")
+last_digit = abs(number) % 10
+print(f"Last digit of {number:d} is {last_digit:d}")
 if last_digit > 5:
     print(" and is greater than 5")
 if last_digit == 0:
