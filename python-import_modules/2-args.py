1baad6e45cba29a80bc246569b54dc286703bf9b
Travis Adamson <tndadamson@gmail.com>
2023-09-06 14:32:21 -0700

    Fixed one part of empty string

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,7 @@ def main():
     num = len(sys.argv) - 1
     if num > 0 and not sys.argv[1]:
         print("0 arguements.")
+        num = -1
     elif num > 1:
         print("{:d} arguments:".format(num))
     elif num == 1:
