05fda088eff859f241261b6c31479cd898f96e02
Travis Adamson <tndadamson@gmail.com>
2023-09-06 10:15:37 -0700

    First push for 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import add(a, b) from add_0.py
+
+def main():
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+
+if __name__ == "__main__":
+    main()
