c66a58c1cfcdd9e57689b2312d46fb59a1fa208a
Travis Adamson <tndadamson@gmail.com>
2023-09-06 14:26:57 -0700

    Still working on eliminating empty strings

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,9 @@ import sys
 
 def main():
     num = len(sys.argv) - 1
-    if not argv[1]:
-        num = 0
-    if num > 1:
+    if num > 0 and not sys.argv[1]:
+        print("0 arguements.")
+    elif num > 1:
         print("{:d} arguments:".format(num))
     elif num == 1:
         print("1 arguement:")
