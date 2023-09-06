9241e84a7b97fa1cdf45c41e7a554c11a94f461e
Travis Adamson <tndadamson@gmail.com>
2023-09-06 07:01:23 -0700

    Changed the other and to or

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i, 10):
-        if j != i and (i != 8 and j != 9):
-            print("{:d}{:d}, ".format(i, j), end="")
+        if j != i:
+            if i != 8 or j != 9:
+                print("{:d}{:d}, ".format(i, j), end="")
 print("{:d}{:d}".format(i, j))
