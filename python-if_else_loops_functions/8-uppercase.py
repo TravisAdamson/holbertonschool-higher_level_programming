f922dc3d4ef0af80e616cacf926a977491c717d1
Travis Adamson <tndadamson@gmail.com>
2023-09-06 07:48:04 -0700

    Fixed logic structure

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        for j in range(97, 123):
-            if ord(str[i]) == j:
-                print("{}".format(str(ord(str[i]) - 32)), end="")
-            else:
-                print("{}".format(str[i]), end="")
+        if ord(str[i]) >= 97 and ord(str[i]) <= 127:
+            print("{}".format(str(ord(str[i]) - 32)), end="")
+        else:
+            print("{}".format(str[i]), end="")
