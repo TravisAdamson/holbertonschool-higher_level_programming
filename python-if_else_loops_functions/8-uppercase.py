a134e051c693509f4d3f63c37a8708bffb5a04cd
Travis Adamson <tndadamson@gmail.com>
2023-09-06 08:06:43 -0700

    Changed variables

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in range(0, len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 127:
-            ni = ord(str[i]) - 32
+            ni = chr(ord(str[i]) - 32)
         else:
-            ni = ord(str[i])
-        print("{}".format(str[ni]), end="")
+            ni = str[i]
+        print("{}".format(ni), end="")
 print()
