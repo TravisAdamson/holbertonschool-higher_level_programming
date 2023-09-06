1ef852e2296d304c6e012b1cdb02fba77bf226f5
Travis Adamson <tndadamson@gmail.com>
2023-09-06 07:36:30 -0700

    Added print statements

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,7 @@
 def uppercase(str):
     for i in range(0, len(str)):
         for j in range(97, 123):
-            if ord(str[i]) == i:
-                str[i] = str(ord(str[i]) - 32) 
+            if ord(str[i]) == j:
+                print("{}".format(str(ord(str[i]) - 32)), end="")
+            else:
+                print("{}".format(str[i]), end="")
