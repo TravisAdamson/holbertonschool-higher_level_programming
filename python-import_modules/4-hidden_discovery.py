a30275606961d7a4542fb15b2a35f73f099cf379
Travis Adamson <tndadamson@gmail.com>
2023-09-06 16:40:25 -0700

    Fixed pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,7 @@ def main():
     if len(dir(hidden_4)) > 0:
         for i in range(0, len(dir(hidden_4))):
             if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
-                    print("{:s}".format(dir(hidden_4)[i]))
+                print("{:s}".format(dir(hidden_4)[i]))
 
 
 if __name__ == "__main__":
