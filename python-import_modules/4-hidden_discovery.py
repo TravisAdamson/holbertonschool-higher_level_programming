5f744442c3681a6f61374d7fc2aefb0ea6df6210
Travis Adamson <tndadamson@gmail.com>
2023-09-06 16:38:48 -0700

    First push for 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-import uncompyle6
-
+import hidden_4
 
 def main():
-    with open("hidden.pyc", "rb") as line:
-        data = f.read()
-    narray = uncompyle6.disassemble(code)
-
-    for name in narray:
-        print("{:s}".format(name))
+    if len(dir(hidden_4)) > 0:
+        for i in range(0, len(dir(hidden_4))):
+            if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
+                    print("{:s}".format(dir(hidden_4)[i]))
 
 
 if __name__ == "__main__":
