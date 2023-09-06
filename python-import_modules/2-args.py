c78d1c7e9dd58ba0b7f97946fc121fa2b4eedc3b
Travis Adamson <tndadamson@gmail.com>
2023-09-06 14:06:27 -0700

    Fixed if logic

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,14 +3,14 @@ import sys
 
 
 def main():
-    num = len(sys.argv)
-    if num != 0 and num > 1:
+    num = len(sys.argv) - 1
+    if num > 1:
         print("{:d} arguments:".format(num))
     elif num == 1:
         print("1 arguement:")
     else:
         print("0 arguements.")
-    if num > 0:
+    if num >= 0:
         for i in range(1, num + 1):
             print("{:d}: {:s}".format(i, sys.argv[i]))
 
