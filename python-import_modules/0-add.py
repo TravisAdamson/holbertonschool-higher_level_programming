84a484b55b57de747f3d166381b8e956a0ae85e6
Travis Adamson <tndadamson@gmail.com>
2023-09-06 10:34:39 -0700

    Trying to remove c variable

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,8 +4,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    c = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, c))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     main()
