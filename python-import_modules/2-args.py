fc67e28cd49c488f9d330c14f344b73a59952ee1
Travis Adamson <tndadamson@gmail.com>
2023-09-06 15:00:18 -0700

    Removed some e's

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,14 +5,14 @@ import sys
 def main():
     num = len(sys.argv) - 1
     if num > 0 and not sys.argv[1]:
-        print("0 arguements.")
+        print("0 arguments.")
         num = -1
     elif num > 1:
         print("{:d} arguments:".format(num))
     elif num == 1:
-        print("1 arguement:")
+        print("1 argument:")
     else:
-        print("0 arguements.")
+        print("0 arguments.")
     for i in sys.argv:
         if sys.argv.index(i) > 0:
             print("{:d}: {:s}".format(sys.argv.index(i), i))
