c3daa3809da5644c5587e47d35dc0eb536e94ab6
Travis Adamson <tndadamson@gmail.com>
2023-09-06 06:30:04 -0700

    Conditional change from or to and

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i, 10):
-        if j != i or (i != 8 and j != 9):
+        if j != i and (i != 8 and j != 9):
             print("{:d}{:d}, ".format(i, j), end="")
 print("{:d}{:d}".format(i, j))
