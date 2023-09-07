4b0a9f415ddb1f8ff947f1c9698db2b30afb3e72
Travis Adamson <tndadamson@gmail.com>
2023-09-07 10:50:11 -0700

    Stuff not met

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
