1a0e3d3e11d2786f409a560339a81cd3cf1c2944
Travis Adamson <tndadamson@gmail.com>
2023-09-08 08:47:14 -0700

    changed formatting variable for pinting

--- python-data_structures/3-main.py
+++ python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [0]
+my_list = [8, 5, 0, 10, 12]
 print_reversed_list_integer(my_list)
