c8f34cc9d62d9e7e1fcd6ecc93f71c1050d02b72
Travis Adamson <tndadamson@gmail.com>
2023-09-07 18:35:23 -0700

    Trying to change number of args

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,20 +3,20 @@ import sys
 import calculator_1
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = ord(sys.argv[2])
     if operator == 43:
-        print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+        print("{} {} {} = {}".format(a, b, calculator_1.add(a, b)))
     elif operator == 45:
-        print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+        print("{} {} {} = {}".format(a, str(operator), b, calculator_1.sub(a, b)))
     elif operator == 42:
-        print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+        print("{} {} {} = {}".format(a, str(operator), b, calculator_1.mul(a, b)))
     elif operator == 47:
-        print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+        print("{} {} {} = {}".format(a, str(operator), b, calculator_1.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
