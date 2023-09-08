4460efa1246206b3a9063276007fa1861d70e969
Travis Adamson <tndadamson@gmail.com>
2023-09-08 12:21:39 -0700

    Fixed loop, again?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     new_i = 0
     new_my_string = ""
     if my_string is not None:
-        for i in range(0, len(my_string) - 1):
+        for i in range(0, len(my_string)):
             if my_string[i] != "c" and my_string[i] != "C":
                 new_my_string += my_string[i]
     return new_my_string
