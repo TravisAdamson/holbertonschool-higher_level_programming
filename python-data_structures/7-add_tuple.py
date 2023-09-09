71db6922b218b7219d45eae8cf8d6ea76d6ea17c
Travis Adamson <tndadamson@gmail.com>
2023-09-09 06:55:00 -0700

    Fixed if checks on tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a=(), tuple_b()):
-    tuple_n = (0, 0)
-    if tuple_a[0] is not None:
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 1:
         a = tuple_a[0]
     else:
         a = 0
-    if tuple_b[0] is not None:
+    if len(tuple_b) >= 1:
         b = tuple_b[0]
     else:
         b = 0
-    tuple_n[0] = a + b
-    if tuple_a[1] is not None:
-        a = tuple_a[1]
+    if len(tuple_a) >= 2:
+        c = tuple_a[1]
     else:
-        a = 0
-    if tuple_b[1] is not None:
-        b = tuple_b[1]
+        c = 0
+    if len(tuple_b) >= 2:
+        d = tuple_b[1]
     else:
-        b = 0
-    tuple_n[1] = a + b
+        d = 0
+    tuple_n = (a + b, c + d)
     return tuple_n
