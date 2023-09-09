9cdd058739d984e8a04c457eff544fcf7a2f570f
Travis Adamson <tndadamson@gmail.com>
2023-09-09 16:03:04 -0700

    Changed for conditions

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,17 +3,11 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
-        for i in my_list:
-            if my_list[i] >= 0:
-                if my_list[i] % 2 is True:
-                    new_list.append(True)
-                else:
-                    new_list.append(False)
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                new_list.append(True)
             else:
-                if abs(my_list[i]) % 2 is True:
-                    new_list.append(True)
-                else:
-                    new_list.append(False)
+                new_list.append(False)
     else:
         return None
     return new_list
