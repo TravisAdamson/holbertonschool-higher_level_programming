7a244290706b3ecd0f03b88675ef2249c8df437f
Travis Adamson <tndadamson@gmail.com>
2023-09-09 12:29:57 -0700

    First push for 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max_i = 0
+    if len(my_list) > 0:
+        for i in range(0, len(my_list):
+                if my_list[i] > max_i:
+                    max_i = my_list[i]
+        return max_i
+    else:
+        return None
