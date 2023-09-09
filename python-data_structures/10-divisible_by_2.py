df2e3ea56b91bc25535a9ba4dfb2331745d5985c
Travis Adamson <tndadamson@gmail.com>
2023-09-09 13:04:28 -0700

    Removed quotes from true false responses

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,9 +5,9 @@ def divisible_by_2(my_list=[]):
     if my_list:
         for i in my_list:
             if my_list[i] % 2 == 0:
-                new_list.append("True")
+                new_list.append(True)
             else:
-                new_list.append("False")
+                new_list.append(False)
     else:
         return None
     return new_list
