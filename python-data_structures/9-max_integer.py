6fd594b19b30d5bdae0ee3783ec1daae6c5f4f26
Travis Adamson <tndadamson@gmail.com>
2023-09-09 12:32:42 -0700

    Fixed missing ) and indention

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 def max_integer(my_list=[]):
     max_i = 0
     if len(my_list) > 0:
-        for i in range(0, len(my_list):
-                if my_list[i] > max_i:
-                    max_i = my_list[i]
+        for i in range(0, len(my_list)):
+            if my_list[i] > max_i:
+                max_i = my_list[i]
         return max_i
     else:
         return None
