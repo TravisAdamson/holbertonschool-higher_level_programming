0586be612f572bd02fec9cf7b2ece3511516742d
Travis Adamson <tndadamson@gmail.com>
2023-09-09 15:23:40 -0700

    Working on negative

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,10 +4,16 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
         for i in my_list:
-            if my_list[i] % 2 == 0:
-                new_list.append(True)
+            if my_list[i] >= 0:
+                if my_list[i] % 2 == 0:
+                    new_list.append(True)
+                else:
+                    new_list.append(False)
             else:
-                new_list.append(False)
+                if abs(my_list[i]) % 2 == 0:
+                    new_list.append(False)
+                else:
+                    new_list.append(True)
     else:
         return None
     return new_list
