859d745b0cde2ac2cf43b29301903bc7a5de9495
Travis Adamson <tndadamson@gmail.com>
2023-09-09 15:48:25 -0700

    Realizing corectly True/False test

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,12 +5,12 @@ def divisible_by_2(my_list=[]):
     if my_list:
         for i in my_list:
             if my_list[i] >= 0:
-                if my_list[i] % 2 == 0:
+                if my_list[i] % 2 == True:
                     new_list.append(True)
                 else:
                     new_list.append(False)
             else:
-                if abs(int((my_list[i]))) % 2 == 0:
+                if abs(my_list[i]) % 2 == True:
                     new_list.append(True)
                 else:
                     new_list.append(False)
