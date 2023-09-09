519734d9339f0af49e364ac502018687b8b1bafa
Travis Adamson <tndadamson@gmail.com>
2023-09-09 12:47:12 -0700

    Fixed issue when all values are negative

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,6 +3,7 @@
 def max_integer(my_list=[]):
     max_i = 0
     if my_list:
+        max_i = my_list[0]
         for i in range(0, len(my_list)):
             if my_list[i] > max_i:
                 max_i = my_list[i]
