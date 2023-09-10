373f0665a7aea7b65a40c573226f86fcec7678c2
Travis Adamson <tndadamson@gmail.com>
2023-09-10 10:42:17 -0700

    Fixed loop to be only the key

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,7 +5,8 @@ def multiply_by_2(a_dictionary):
         return None
     new_value = 0
     new_dictionary = {}
-    for key, value in a_dictionary:
+    for key in a_dictionary:
+        value = a.dictionary.get(key)
         new_value = value * 2
         new_dictionary[key] = new_value
     return new_dictionary
