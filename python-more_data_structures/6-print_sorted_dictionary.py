39907a6e94aa190673b874e584ca8af229369c39
Travis Adamson <tndadamson@gmail.com>
2023-09-10 09:58:49 -0700

    Fixed naming

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    my_sorted_keys = sorted(dict.items(), key=lambda x: x[0])
+    my_sorted_keys = sorted(a_dictionary.items(), key=lambda x: x[0])
     for key, value in my_sorted_keys:
         print("{:s}: {}".format(key, value))
