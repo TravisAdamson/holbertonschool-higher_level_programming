290472fc7f355fcf795798ae3be697726bfdbfa0
Travis Adamson <tndadamson@gmail.com>
2023-09-10 06:54:00 -0700

    Fixed loop ranges

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,8 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for i in matrix:
+    for i in range(len(matrix)):
         new_matrix.append([])
-        for i2 in i:
+        for i2 in matrix[i]:
             new_matrix[i].append(i2 * i2)
     return new_matrix
