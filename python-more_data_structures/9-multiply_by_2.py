09204dda8b4fa9a4c0fdaf9a69412e799f22b14c
Travis Adamson <tndadamson@gmail.com>
2023-09-10 10:42:56 -0700

    Fixed typo

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -6,7 +6,7 @@ def multiply_by_2(a_dictionary):
     new_value = 0
     new_dictionary = {}
     for key in a_dictionary:
-        value = a.dictionary.get(key)
+        value = a_dictionary.get(key)
         new_value = value * 2
         new_dictionary[key] = new_value
     return new_dictionary
