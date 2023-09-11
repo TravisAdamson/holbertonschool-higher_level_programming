967686164a2226bbf2a886d2929d656eba615b9d
Travis Adamson <tndadamson@gmail.com>
2023-09-11 08:25:04 -0700

    Fixed making sure the subtractions are needed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -25,15 +25,29 @@ def roman_to_int(roman_string):
     conv_value = 0
     temp_1 = 0
     temp_2 = 0
+    flag = 0
+    set_I = ('V', 'X')
+    set_X = ('L', 'C')
+    set_C = ('D', 'M')
     for i in range(len(roman_string)):
+        if roman_string[i] == 'I' and i + 1 < len(roman_string):
+            if roman_string[i + 1] in set_I:
+                flag = 1
+        if roman_string[i] == 'X' and i + 1 < len(roman_string):
+            if roman_string[i + 1] in set_X:
+                flag = 1
+        if roman_string[i] == 'C' and i + 1 < len(roman_string):
+            if roman_string[i + 1] in set_C:
+                flag = 1
         temp_1 = get_dec_value(roman_string[i])
         if temp_1 == -1:
             return 0
-        if i + 1 < len(roman_string):
+        if flag = 1:
             temp_2 = get_dec_value(roman_string[i + 1])
             if temp_2 == -1:
                 return 0
             if temp_1 < temp_2:
+
                 conv_value += temp_2 - temp_1
                 i += 1
                 if i + 1 >= len(roman_string):
@@ -42,4 +56,5 @@ def roman_to_int(roman_string):
                 conv_value += temp_1
         else:
             conv_value += temp_1
+        flag = 0
     return int(conv_value)
