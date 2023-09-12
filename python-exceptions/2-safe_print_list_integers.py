90603170d2f8ba39ea33d7a31cbbc29a6ac0a876
Travis Adamson <tndadamson@gmail.com>
2023-09-12 07:05:59 -0700

    First push for 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            print("{:d}".format(i), end="")
+            count += 1
+        except TypeError as inv_type:
+            pass
+        except IndexError as ind_err:
+            raise
+    print()
+    return count
