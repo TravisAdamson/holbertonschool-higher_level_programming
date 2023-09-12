62880df010105347a7b4d724f42e6d7a5e1f6094
Travis Adamson <tndadamson@gmail.com>
2023-09-12 09:44:12 -0700

    First push for 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    res_div = 0
+    try:
+        res_div = a / b
+    except Exception as ex:
+        res_div = None
+    finally:
+        print("Inside result: {}".format(res_div))
+        return res_div
