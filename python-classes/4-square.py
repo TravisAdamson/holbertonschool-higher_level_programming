11fe7378828d10937bf18df113a7924b8443032a
Travis Adamson <tndadamson@gmail.com>
2023-09-14 07:31:17 -0700

    Added getter and setter indicators

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,30 +13,18 @@ class Square:
         Args:
             size (int): The size of said new square.
         """
-        if type(size) == int:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
-            raise TypeError("size must be an integer")
+        self.__size = size
 
-    def area(self):
-        """Calculates the area of the given square
-
-        Args:
-            self (square): THe square to calculate with.
-        """
-        return (self.__size * self.__size)
-
+    @property
     def size(self):
         """Retrieves the size of Square
 
         Args:
             self (square): The square to get the size of
         """
-        return self.__size
+        return (self.__size)
 
+    @size.setter
     def size(self, value):
         """Sets the size value of the Square
 
@@ -44,10 +32,15 @@ class Square:
             self (square): The given Square
             value (int): The size of to set to Square.
         """
-        if type(value) == int:
-            if value >= 0:
-                self.__size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the given square
+
+        Args:
+            self (square): THe square to calculate with.        """
+        return (self.__size * self.__size)
