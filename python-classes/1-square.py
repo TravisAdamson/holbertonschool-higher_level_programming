37300b34eae05cfb6a3b913639cd6c211df1578b
Travis Adamson <tndadamson@gmail.com>
2023-09-14 06:06:09 -0700

    Adding another square

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-square.py
+# Travis Adamson
+""" Define a class "Square". """
+
+
+class Square:
+    """This is an empty class to define square"""
+
+    def __init__(self, size):
+        """Initializes a new "Square".
+
+        Args:
+            size (int): The size of said new square.
+        """
+        self.__size = size
