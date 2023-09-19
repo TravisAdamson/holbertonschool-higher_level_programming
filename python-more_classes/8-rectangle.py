30f5d5c0f64c0b4d369b14ba70185557bb97a554
Travis Adamson <tndadamson@gmail.com>
2023-09-19 05:17:58 -0700

    Still chasing req

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -115,7 +115,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return (rect_1)
-        return(rect_2)
+        return (rect_2)
 
     def __str__(self):
         """Return the printable version of the Rectangle.
