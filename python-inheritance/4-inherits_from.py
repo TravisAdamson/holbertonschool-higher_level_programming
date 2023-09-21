487074930ac10209e1f19b84d9a2f3a070a152a7
Travis Adamson <tndadamson@gmail.com>
2023-09-21 15:47:31 -0700

    Adding 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+# Travis Adamson
+""" Defines a function to determine if obj inherited the given class """
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj inherits from a_class
+
+    Args:
+        obj (any): The given object to check
+        a_class (any): The class to compare to
+
+    Returns: True if inherits otherwise False
+    """
+    if issubclass(obj.__class__, a_class):
+        return True
+    else:
+        return False
