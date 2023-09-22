00c14feb58d028eb1843ecfa987d39d5eca55f3d
Travis Adamson <tndadamson@gmail.com>
2023-09-22 12:37:59 -0700

    Adding 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-write_file.py
+# Travis Adamson
+""" Defines a function to write to a file """
+
+
+def write_file(filename="", text=""):
+    """Writes the given text to the given file
+
+    Args:
+        filename (file): The file to write to.
+        text (string): The text to write to the file
+    """
+    if filename != "" and text != "":
+        with open(filename, "w", encoding="utf-8") as cur_file:
+            cur_file.write(text)
