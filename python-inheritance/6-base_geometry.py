0a2ee650edf8b510f3479c72682008a0dc1033d0
Travis Adamson <tndadamson@gmail.com>
2023-09-21 19:18:06 -0700

    Adding 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0-square.py
+# Travis Adamson
+""" Define a class "BaseGeometry". """
+
+
+class BaseGeometry:
+    """This is an empty class to define BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
