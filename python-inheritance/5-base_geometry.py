59a0da66d26e8f88abec9f9d898548b3d90f00d9
Travis Adamson <tndadamson@gmail.com>
2023-09-21 19:16:06 -0700

    Adding 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-square.py
+# Travis Adamson
+""" Define a class "BaseGeometry". """
+
+
+class BaseGeometry:
+    """This is an empty class to define BaseGeometry"""
+
+    pass
