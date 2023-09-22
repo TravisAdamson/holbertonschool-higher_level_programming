217a3a1870b747ae6f639f73a8f1e22a76c8771b
Travis Adamson <tndadamson@gmail.com>
2023-09-22 08:13:31 -0700

    Trying something different for 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,32 +1,8 @@
 #!/usr/bin/python3
-# 0-square.py
+# 8-rectangle.py
 # Travis Adamson
-""" Define a class "BaseGeometry". """
-
-
-class BaseGeometry:
-    """This is an empty class to define BaseGeometry"""
-
-    def area(self):
-        """ Not impletemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates an integer
-
-        Args:
-            name (str): The name of the parameter
-            value (int): The value to validate
-        Raises:
-            TypeError: If value is not an integer
-            ValueError: If value is <= 0.
-        """
-        te_msg = name + " must be an integer"
-        ve_msg = name + " must be greater than 0"
-        if type(value) != int:
-            raise TypeError(te_msg)
-        if value <= 0:
-            raise ValueError(ve_msg)
+""" Define a class "Rectangle" using "BaseGeometry". """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
