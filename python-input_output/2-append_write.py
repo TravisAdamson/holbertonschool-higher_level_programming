a6d0f60f1d5409523d7b99c77e91f359d9e63357
Travis Adamson <tndadamson@gmail.com>
2023-09-22 12:43:41 -0700

    Fixed function name

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# 1-write_file.py
+# 2-append_write.py
 # Travis Adamson
 """ Defines a function to append to a file """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Appends the given text to the given file
 
     Args:
