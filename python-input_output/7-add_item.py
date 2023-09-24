2e7e4082eb625ef1547ce775901a1ca8a9b30065
Travis Adamson <tndadamson@gmail.com>
2023-09-24 12:06:34 -0700

    Fixing indents

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    new_list = load_from_json_file("add_item.json")
-except FileNotFoundError as fnf:
-    new_list = []
-new_list.extend(sys.argv[1:])
-save_to_json_file(new_list, "add_item.json")
+    try:
+        new_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        new_list = []
+    new_list.extend(sys.argv[1:])
+    save_to_json_file(new_list, "add_item.json")
