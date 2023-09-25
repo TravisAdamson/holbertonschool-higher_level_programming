29440b3bfcc6147cde59c58736929a4714b12cb8
Travis Adamson <tndadamson@gmail.com>
2023-09-25 05:29:01 -0700

    Adding 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# 11-student.py
+# Travis Adamson
+""" Defines a class Student"""
+
+
+class Student:
+    """The class for student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new object student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary rep of a student
+
+        If attrs is a list of strings, represents only
+        attributes included in that list.
+
+        Args:
+            attrs (list): (Optional) Attributes to include.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Load all attributes of the student from json
+
+        Args:
+            json (dict): The key/value pairs to use
+        """
+        for i, j in json.items():
+            setattr(self, i, j)
