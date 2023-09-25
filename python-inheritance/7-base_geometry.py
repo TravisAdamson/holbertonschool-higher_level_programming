636f1c75e7b9e473fc30ee3a32ccb5e494f6a269
Travis Adamson <tndadamson@gmail.com>
2023-09-25 08:23:47 -0700

    Testing different doctest file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
 
     def area(self):
         """This is not implemented yet"""
-        raise Exception("area() has not been implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validates an integer
