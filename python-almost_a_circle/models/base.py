771a863bd8f5d8d4afd27fc5e15f7cc8c1f02568
Travis Adamson <tndadamson@gmail.com>
2023-09-28 13:47:47 -0700

    Adding json data

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 # base.py
 # Travis Adamson
 """ Declares a new base class """
+import json
 
 
 class Base:
@@ -19,3 +20,76 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON version of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns an object from the json string provided
+
+        Args:
+            json_string (str): A Json string of a list of dictionaries
+        Returns:
+            Empty list or object represented by the string
+        """
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class created from dictionary of attribs
+
+        Args:
+            **dictionary (dict): Key/value pairs for attibs for init
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new_obj = cls(1, 1)
+            else:
+                new_obj = cls(1)
+            new_obj.update(**dictionary)
+            return new_obj
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write json rep of a list of objects to a file
+
+        Args:
+            list_objs (list): A list of Base inherited objects
+        """
+        filename = cls.__name__ + ".json"
+        list_dictionaries = {}
+        with open(filename, "w") as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                for objs in list_objs:
+                    list_dictionaries += objs.to_dictionary()
+                json_file.write(Base.to_json_string(list_dictionaries))
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of classes created from json strings
+
+        Reads from <cls.__name__>.json
+
+        Returns:
+            Empty list or a list of created classes
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as json_file:
+                list_dictionaries = Base.from_json_string(json_file.read())
+                return [cls.create(**d) for d in list_dictionaries]
+        except IOError:
+            return []
