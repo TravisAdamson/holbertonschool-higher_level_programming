55d571e312f76f959fc41536f4153c529dbdb553
Travis Adamson <tndadamson@gmail.com>
2023-09-28 12:58:25 -0700

    Adding testing

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,6 +30,47 @@ class Square(Rectangle):
         self.width = value
         self. height = value
 
+    def update(self, *args, **kwargs):
+        """Update the Square:
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument is id attribute
+                - 2nd argument is size attribute
+                - 3rd argument is x attribute
+                - 4th argument is y attribute
+            **kwargs (dict): New pairs of atributes using key/value
+        """
+        if args and len(args) != 0:
+            count = 0
+            for argument in args:
+                if count == 0:
+                    if argument is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = argument
+                elif count == 1:
+                    self.size = argument
+                elif count == 2:
+                    self.x = argument
+                elif count == 3:
+                    self.y = argument
+                count += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+    
     def __str__(self):
         """Return the rep of square for print() and str()"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
