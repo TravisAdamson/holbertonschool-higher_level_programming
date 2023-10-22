962840caf5b887aac08210c8c0e97638d086631e
Travis Adamson <tndadamson@gmail.com>
2023-10-22 09:09:14 -0500

    Adding 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all genres from hbtn_0d_tvshows and displays shows linked to each other
+SELECT tv_genres.name AS genre,
+COUNT(show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
