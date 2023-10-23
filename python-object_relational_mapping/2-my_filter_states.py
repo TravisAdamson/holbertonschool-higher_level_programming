d5d21168d427cc47609351a61817e9203c4da8d5
Travis Adamson <tndadamson@gmail.com>
2023-10-23 07:52:22 -0500

    Adding 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Contains MySQLdb script that prints states starting with given letter"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        if state[1][0] == sys.argv[4][0]:
+            print(state)
+
+    cursor.close()
+    db.close()
