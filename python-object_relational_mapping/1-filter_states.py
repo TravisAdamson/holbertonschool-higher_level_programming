0b598f683c039c752ef070e170a8e34e843216d6
Travis Adamson <tndadamson@gmail.com>
2023-10-23 07:36:34 -0500

    Fixing flake8 and changing method

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Contains MySQLdb script that gets all states that start with "N" from given table """
+""" Contains MySQLdb script that gets all states that start with "N" """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
+    cur.execute("SELECT * FROM states WHERE state LIKE 'N%' ORDER BY id")
+    states = cur.fetchall()
 
     for state in states:
-        if state[1][0] == 'N':
-            print(state)
+        print(state)
 
-    cursor.close()
+    cur.close()
     db.close()
