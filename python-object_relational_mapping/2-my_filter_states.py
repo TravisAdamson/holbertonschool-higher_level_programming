d20c12fa9abdbb408e7fb2440e1bbbd6d690b3bf
Travis Adamson <tndadamson@gmail.com>
2023-10-23 08:02:10 -0500

    Testing different version for 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,13 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = '{}' "
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}%' "
                    "ORDER BY id ASC".format(sys.argv[4]))
 
     states = cursor.fetchall()
 
     for state in states:
-        if state[1] == sys.argv[4]:
-            print(state)
+        print(state)
 
     cursor.close()
     db.close()
