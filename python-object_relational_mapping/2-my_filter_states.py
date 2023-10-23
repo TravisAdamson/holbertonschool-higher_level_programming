00f13e424878d03a180fcefb5253ba6691050076
Travis Adamson <tndadamson@gmail.com>
2023-10-23 08:05:25 -0500

    Trying more other ways for 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}%' "
+    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE '{}%' "
                    "ORDER BY id ASC".format(sys.argv[4]))
 
     states = cursor.fetchall()
