d9e911f029fe60c0669f725644b6456c899f8e6f
Travis Adamson <tndadamson@gmail.com>
2023-10-24 07:08:52 -0500

    Adding 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Contains MySQLdb script that prints states starting with given letter"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE '{}%' "
+                   "ORDER BY id ASC".format(sys.argv[4]))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+
+    cursor.close()
+    db.close()
