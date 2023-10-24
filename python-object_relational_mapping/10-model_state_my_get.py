f3e38bcb80f8bc6e24bb13bbd17f46eb12721055
Travis Adamson <tndadamson@gmail.com>
2023-10-24 08:15:42 -0500

    Adding 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Prints the the States  with an a from the database hbtn_0e_6_usa """
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not Found")
