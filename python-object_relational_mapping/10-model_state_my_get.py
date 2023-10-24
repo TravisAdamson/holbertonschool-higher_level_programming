2d1e077cfb37a8323e1660932b9f7383307f2a40
Travis Adamson <tndadamson@gmail.com>
2023-10-24 08:18:51 -0500

    Changed to single quotes in final print?

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Prints the the States  with an a from the database hbtn_0e_6_usa """
+""" Prints the the given state from the database hbtn_0e_6_usa """
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
                            @localhost:3306/{argv[3]}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     if state is not None:
         print("{}".format(state.id))
     else:
-        print("Not Found")
+        print('Not Found')
