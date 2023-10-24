22cdf76015ed986c3f86ef714876c6f1a35ab331
Travis Adamson <tndadamson@gmail.com>
2023-10-24 07:28:43 -0500

    Fixing injection issue

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,13 +6,12 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
+    c = db.cursor()
 
-    cursor.execute('''SELECT cities.id, cities.name, states.name
-                   FROM cities
-                   JOIN states ON cities.state_id = states.id
-                   ORDER BY cities.id ASC;
-                   ''')
+    queery = '''SELECT cities.id, cities.name, states.name FROM cities 
+                JOIN states ON cities.state_id = states.id 
+                ORDER BY cities.id'''
+    c.execute(queery)
 
     cities = cursor.fetchall()
 
