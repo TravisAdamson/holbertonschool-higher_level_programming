b6d691816023ae14d78fcdcf481f406885ad67d3
Travis Adamson <tndadamson@gmail.com>
2023-11-23 16:03:23 -0600

    Fixed conditional

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const responseZero = 'No argument';
 const args = process.argv.slice(2);
-if (args[0] === null) {
+if (args[0] === undefined) {
   console.log(responseZero);
 } else {
   console.log(args[0]);
