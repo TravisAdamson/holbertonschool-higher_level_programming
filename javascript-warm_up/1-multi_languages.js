4f75a6d05f9d92f962497fc70bbf3d8f2ecbf1f1
Travis Adamson <tndadamson@gmail.com>
2023-11-22 19:49:39 -0600

    Fixed name of file

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const thePhrase1 = 'C is fun';
+const thePhrase2 = 'Python is cool';
+const thePhrase3 = 'JavaScript is amazing';
+console.log(thePhrase1 + '\n' + thePhrase2 + '\n' + thePhrase3);
