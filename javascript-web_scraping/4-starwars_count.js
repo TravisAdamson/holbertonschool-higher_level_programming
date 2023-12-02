cd666d434df1b13e78e78daa283872238fa6ab3b
Travis Adamson <tndadamson@gmail.com>
2023-12-02 08:53:15 -0600

    Searching for a particular character within the api

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const theAnswer = JSON.parse(body).results;
+    console.log(theAnswer.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
